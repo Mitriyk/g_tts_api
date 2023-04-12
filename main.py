f09db64d1c769dc3d5671ee3a4b3cf0725eed82f
Дмитрий <Mitriyk@gmail.com>
2023-04-12 18:06:21 +0300

    first work wersion

---
+++ main.py
@@ -0,0 +1,17 @@
+import gtts
+import PyPDF2
+
+file_name = 'test_ru.pdf'
+lang = 'ru'
+
+with open(file_name, 'rb') as file:
+    pdf_file = PyPDF2.PdfReader(file)
+    text = pdf_file.pages[0].extract_text()
+
+print('[+] Processing...')
+t1 = gtts.gTTS(text, lang='ru')
+t1.save('audio.mp3')
+print('[+] Audio file saved')
+
+
+# Перехватывает сообщения и прочитывает их
